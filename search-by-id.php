b678e5d31b522d22d21478a76ea2f9c2c70f3cbf
wpkonsulent <wpkonsulent@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-09-25 11:06:21 +0000

    git-svn-id: https://plugins.svn.wordpress.org/search-by-id/trunk@603554 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ search-by-id.php
@@ -0,0 +1,43 @@
+<?php
+/*
+	Plugin Name: Search by ID
+	Plugin URI: 
+	Description: Enables the user to search by post ID using the built-in search within the administration area. Works for all kinds of posts (posts, pages, custom post types).
+	Version: 1.0
+	Author: Uffe Fey, WordPress consultant
+	Author URI: http://wpkonsulent.dk
+*/
+	$wpkonsulent_searchbyid = new WPkonsulentSearchById();
+	
+	class WPkonsulentSearchById
+	{
+		function WPkonsulentSearchById()
+		{
+			$this->__construct();
+		}
+
+		function __construct()
+		{
+			add_filter('posts_where', array(&$this, 'posts_where'));
+		}
+		
+		function posts_where($where)
+		{
+			if(is_admin() && is_search())
+			{
+				$s = $_GET['s'];
+				
+				if(!empty($s))
+				{
+					if(is_numeric($s))
+					{
+						global $wpdb;
+						$where .= ' or ' . $wpdb->posts . '.ID = ' . $s;
+					}
+				}
+			}
+			
+			return $where;
+		}
+	}
+?>
